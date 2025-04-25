d466981a2c17f5048cbd0c5a2211f2c8001f6e37
Muskan Khandelwal <35535203+MuskanKhandelwal@users.noreply.github.com>
2025-04-25 11:45:08 -0400

    Update Add Two Numbers.py

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -5,6 +5,7 @@ class ListNode:
         self.next = next
 
 class Solution:
+    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode()
         curr=dummy
