8a6dc92e45b871992595abb3d8db3cde6bf3357c
Muskan Khandelwal <35535203+MuskanKhandelwal@users.noreply.github.com>
2025-04-27 11:46:32 -0400

    Update Add Two Numbers.py

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -3,7 +3,6 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 class Solution:
     
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
