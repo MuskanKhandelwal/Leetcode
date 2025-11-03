4902f531cad6bc88b5033e72284f39e712304b94
Muskan Khandelwal <35535203+MuskanKhandelwal@users.noreply.github.com>
2025-11-03 10:11:06 -0500

    Update Linked List Cycle Detection.py

--- Linked List Cycle Detection.py
+++ Linked List Cycle Detection.py
@@ -17,3 +17,15 @@ class Solution:
         return False
 
         
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        s,f = head, head
+
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+            if s == f:
+                return True
+        return False
+
+#slow and fast pointers pattern 
