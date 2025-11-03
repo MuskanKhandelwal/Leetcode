26fae407e63f3eded801a0b55a758575bf718796
Muskan Khandelwal <35535203+MuskanKhandelwal@users.noreply.github.com>
2025-11-03 06:47:22 -0500

    Update Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -13,3 +13,20 @@ class Solution:
             prev=curr
             curr=temp
         return prev
+
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        nexti = None
+        while curr:
+            nexti = curr.next 
+            curr.next = prev
+            prev = curr
+            curr = nexti
+        return prev
+
+
+#Need 3 pointers for the solution prev, curr, next 
